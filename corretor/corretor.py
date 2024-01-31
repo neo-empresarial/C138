60f59b229cad10929b2c55adda6a44db04132f83
GUSNEO <gutobevtorres@gmail.com>
2024-01-31 15:35:12 -0300

    adding function to paint cells

--- corretor/corretor.py
+++ corretor/corretor.py
@@ -5,9 +5,9 @@ nltk.download('punkt')
 with open('corretor/treinamento.txt', 'r', encoding='utf-8') as f:
     treinamento = f.read()
 
-# print(treinamento[:500])
+# #print(treinamento[:500])
 
-# print(len(treinamento))
+# #print(len(treinamento))
 
 texto_exemplo = 'Olá, tudo bem?'
 
@@ -23,13 +23,13 @@ def separa_palavras(lista_tokens): ## função para separar somente palavras, ti
     return lista_palavras
 
 palavras_separadas = separa_palavras(palavras_separadas)
-# print(palavras_separadas)
+# #print(palavras_separadas)
 
 lista_tokens = nltk.tokenize.word_tokenize(treinamento)
 
 lista_palavras = separa_palavras(lista_tokens) ## lista_palavras armazena todas as palavras do treinamento tokenizadas, sem caracteres especiais
 
-# print(len(lista_palavras))
+# #print(len(lista_palavras))
 
 def normalizacao(lista_palavras): ## função para normalizar as palavras, deixando todas em minúsculo
     lista_normalizada = []
@@ -38,7 +38,7 @@ def normalizacao(lista_palavras): ## função para normalizar as palavras, deixa
     return lista_normalizada
 
 lista_normalizada = normalizacao(lista_palavras) ## lista_normalizada armazena todas as palavras do treinamento tokenizadas, sem caracteres especiais e em minúsculo
-print(lista_normalizada[:10])
+#print(lista_normalizada[:10])
 
-print(len(set(lista_normalizada))) ## set() retorna somente os valores únicos da lista, logo esse print retorna o total de palavras únicas do treinamento
+#print(len(set(lista_normalizada))) ## set() retorna somente os valores únicos da lista, logo esse print retorna o total de palavras únicas do treinamento
 
